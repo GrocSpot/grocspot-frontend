6533e83a380fde18d662bbc30194fe93639d6c37
Apoorva-Sati <apoorva.sati.professional@gmail.com>
2026-06-17 12:01:16 +0530

    tailwind installed

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from 'react-native';
+import './global.css';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello GrocSpot</Text>
-    </View>
+    <ScrollView className="flex-1 bg-gray-50">
+      <View className="flex-1 items-center justify-center p-4">
+        <Text className="text-2xl font-bold text-gray-800 mb-4">Welcome to GrocSpot!</Text>
+      </View>
+    </ScrollView>
   );
 }
